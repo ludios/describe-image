6d41fb43655440b120af7c3bd3a805fe22283dc0
Ivan Kozik <ivan@ludios.org>
2020-08-24 21:52:57 +0000

    Add docker-demo.sh

---
+++ docker-demo.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -eu
+
+chmod -R a+rX $HOME/code/web-capture/learn-playwright
+docker container run \
+	--mount type=bind,source=$HOME/code/web-capture/learn-playwright,target=/app \
+	--rm \
+	--ipc=host \
+	--security-opt seccomp=$HOME/code/web-capture/playwright/docs/docker/seccomp_profile.json \
+	playwright:localbuild \
+	node /app/demo.js "https://www.google.com"
